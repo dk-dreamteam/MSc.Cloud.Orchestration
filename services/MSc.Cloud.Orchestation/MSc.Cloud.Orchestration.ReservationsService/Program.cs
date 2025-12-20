1b64e6389c91e9eac3e406cde973cffb37648106
dimgrev <dim.grev@outlook.com>
2025-12-20 17:47:34 +0200

    added swagger for reservation service

--- services/MSc.Cloud.Orchestation/MSc.Cloud.Orchestration.ReservationsService/Program.cs
+++ services/MSc.Cloud.Orchestation/MSc.Cloud.Orchestration.ReservationsService/Program.cs
@@ -18,11 +18,9 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
